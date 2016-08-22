327f33b6c0ff67ebc0eab5e5eb83d3c8167b5281
Chie <akiba.chie@gmail.com>
2016-08-22 15:21:17 -0700

    Still can't run this for some reason

--- index.js
+++ index.js
@@ -14,32 +14,29 @@ var Person = function () {
     var name = 'Derek Zoolander';
     var imageUrl = 'http://uifaces.com/assets/static/images/zoolander/jpg';
     var job = 'Male model';
-    return ( < div className = 'person' >
-        < div className = 'person-name' > {
-            name
-        } < /div> < img className = 'person-img'
-        src = {
-            imageUrl
-        } >
-        < div className = 'person-job' > {
-            job
-        } < /div> < /div >
+    return (
+        <div className = 'person'>
+            <div className = 'person-name'>{name}</div>
+            <img className = 'person-img' src = {imageUrl}/>
+            <div className = 'person-job'>{job}</div>
+        </div >
     );
 };
 
 var PersonList = function () {
-    return ( < div className = "person-list" >
-        < Person / >
-        < Person / >
-        < Person / >
-        < Person / >
-        < Person / >
-        < /div>
+    return (
+        <div className = "person-list">
+            <Person/>
+            <Person/>
+            <Person/>
+            <Person/>
+            <Person/>
+        </div>
     );
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    ReactDOM.render( < PersonList / > , document.getElementById('app'));
+    ReactDOM.render( <PersonList/> , document.getElementById('app'));
 });
 
 server.listen(process.env.PORT || 8080);
